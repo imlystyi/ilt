fcc390238f3af8e77bc34a10b49bb38553282b6a
Vladyslav Yakubovskyi <vladte3c@gmail.com>
2023-07-30 14:27:15 +0300

    Inserted license text into files

--- application/iltshell.py
+++ application/iltshell.py
@@ -1,10 +1,26 @@
+# ilt - insert license text
+# Copyright (C) 2023  imlystyi
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
 A simple shell for the "ilt" application.
+
 """
+import iltlib as lib
 from cmd import Cmd
 from enum import Enum
 from re import findall
-import iltlib as lib
 from iltexceptions import *
 
 
